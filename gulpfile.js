6c658c4a2de51216af67b120897ca1d3286e66f7
Fernanda Feliciano <fernandaregina.feliciano@gmail.com>
2015-02-16 01:32:57 -0200

    correções no gulpfile e atualizações nos arquivos .styl

--- gulpfile.js
+++ gulpfile.js
@@ -7,21 +7,37 @@ gulp.dest(pasta): diz para onde o arquivo final precisa ir
 */
 
 //packages
-var gulp        = require('gulp'),
-		plumber     = require('gulp-plumber'), //recebe erros e nao mata o gulp
-		minifycss 	= require('gulp-minify-css'), //minificador de css
-		stylus      = require('gulp-stylus'), //compilador de stylus
-		uglify      = require('gulp-uglify'), //minificador de JS
-		concat      = require('gulp-concat'), //concatenador de JS
-		imagemin    = require('gulp-imagemin'); //otimizador de imagens
+var gulp          = require('gulp'),
+		plumber       = require('gulp-plumber'), //recebe erros e nao mata o gulp
+		minifycss 	  = require('gulp-minify-css'), //minificador de css
+		stylus        = require('gulp-stylus'), //compilador de stylus
+    prefixer      = require('autoprefixer-stylus'),
+    uglify        = require('gulp-uglify'), //minificador de JS
+  	concat        = require('gulp-concat'), //concatenador de JS
+  	imagemin      = require('gulp-imagemin'), //otimizador de imagens
+  	cmq 				  = require('gulp-combine-media-queries');
+
 //compila o stylus e minifica o css
 gulp.task('stylus', function(){
-	gulp.src('./public/gulp/stylus/**/*.styl')
+	return gulp.src('./public/gulp/stylus/style.styl')
 	.pipe(plumber())
-	.pipe(stylus())
+	.pipe(stylus({
+    use:[prefixer()]
+  }))
+  .pipe(concat('style.css'))
 	.pipe(minifycss())
 	.pipe(gulp.dest('./public/css/'))
 });
+
+//organiza os media-query no final do arquivo <3
+gulp.task('cmq', function () {
+	return gulp.src('./public/css/')
+	.pipe(cmq({
+		log: true
+	}))
+	.pipe(gulp.dest('dist'));
+});
+
 //concatena e minifica os JS
 gulp.task('js', function(){
 	return gulp.src('./public/gulp/js/**/*.js')
@@ -31,6 +47,7 @@ gulp.task('js', function(){
 	.pipe(gulp.dest('./public/js/'))
 });
 
+//otimiza imagens
 gulp.task('imagemin', function() {
 	return gulp.src('./public/gulp/img/**/*')
 	.pipe(plumber())
@@ -38,10 +55,11 @@ gulp.task('imagemin', function() {
 	.pipe(gulp.dest('./public/images/'))
 });
 
+//acompanha as mudanças e reexecuta as funcoes
 gulp.task('watch', function () {
 	gulp.watch('./public/gulp/stylus/**/*.styl', ['stylus']);
 	gulp.watch('./public/gulp/js/**/*.js', ['js']);
 	gulp.watch('./public/gulp/img/**/*.{jpg,png,gif}', ['imagemin']);
 });
 
-gulp.task('default', ['js', 'stylus', 'imagemin', 'watch']);
+gulp.task('default', ['js', 'stylus', 'cmq', 'imagemin', 'watch']);
